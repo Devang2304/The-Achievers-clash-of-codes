d4c8c070f08d0cc562e9b85a58dfa10308eb8576
Devang2304 <devangvartak2304@gmail.com>
2023-03-04 23:00:03 +0530

    working chatwindow bug resolved

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-const express=require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
 
-const http=require('http').createServer(app);
+const PORT = process.env.PORT || 3000
 
-const PORT= process.env.PORT || 3000;
-
-
-http.listen(PORT,()=>{
-    console.log(`Listening on port http://localhost:${PORT}`);
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+// Socket 
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+
 })
